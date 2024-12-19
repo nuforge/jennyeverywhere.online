6f22da273fec5c9ac385f7c946bad1b7610813fa
nuForge <nuforge@gmail.com>
2024-12-19 11:19:30 -0500

    Render Markdown and add icons to links

---
+++ src/components/VTag.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-chip tile class="rounded-lg" variant="text">
+    <template v-slot:prepend><v-icon :icon="icon" :color="color"></v-icon></template>
+    <template v-slot:default><span class="mx-2 text-no-wrap">{{ text }}</span></template>
+    <template v-slot:append v-if="value"><v-badge :content="value" inline :color="color"></v-badge></template>
+  </v-chip>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  prependIcon: String,
+  appendIcon: String,
+  icon: String,
+  text: String,
+  class: String,
+  color: String,
+  value: String
+})
+</script>

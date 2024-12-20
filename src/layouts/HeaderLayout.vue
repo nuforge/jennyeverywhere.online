6c35502c3e95c6eade2542dfaed40e6f47648cb3
nuForge <nuforge@gmail.com>
2024-12-20 17:42:03 -0500

    refactor: Split story and tag stores

--- src/layouts/HeaderLayout.vue
+++ src/layouts/HeaderLayout.vue
@@ -5,8 +5,7 @@
       <router-link to="/"><img :src="JELogo" max-height="60" /></router-link>
     </v-app-bar-title>
     <template v-slot:append>
-      <v-btn to="/about">About</v-btn>
-      <v-btn to="/character">character</v-btn>
+      <v-btn to="/character" prepend-icon="mdi-account-circle">Jenny Everywhere</v-btn>
       <v-btn :icon="props.theme === 'dark' ? 'mdi-weather-night' : 'mdi-weather-sunny'" slim
         @click="$emit('change-theme')" color="text" size="small"></v-btn>
       <v-btn :prepend-icon="momentum.icon()" @click="momentum.increment()" color="text"

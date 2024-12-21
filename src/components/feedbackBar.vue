7db9142e896024fb5bb395852c4ca9616ca3ebca
nuForge <nuforge@gmail.com>
2024-12-20 20:04:32 -0500

    add sheets and init bottom drawer

--- src/components/feedbackBar.vue
+++ src/components/feedbackBar.vue
@@ -2,11 +2,17 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-btn v-for="(action, index) in actions" :key="index" icon :color="action.color" density="compact"
-          variant="plain" :ripple="false" :class="action.active ? 'opacity-100' : 'opacity-20'"
-          @click="action.active = !action.active">
-          <v-icon :icon="action.icon"></v-icon>
-        </v-btn>
+        <v-btn-toggle v-model="selected" selected-class="opacity-100">
+          <v-btn v-for="(vote, index) in votes" :key="index" :icon="vote.icon" :color="vote.color" density="compact"
+            class="opacity-10" variant="plain" :ripple="false">
+          </v-btn>
+        </v-btn-toggle>
+        <v-btn-toggle v-model="acted" selected-class="opacity-100" multiple>
+          <v-btn v-for="(action, index) in actions" :key="index" :color="action.color" density="compact" variant="plain"
+            class="opacity-10" :ripple="false" multiple :icon="`${action.icon}`">
+          </v-btn>
+        </v-btn-toggle>
+
       </v-col>
     </v-row>
   </v-container>
@@ -15,27 +21,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const actions = ref({
+const selected = ref<string[]>([]);
+const acted = ref<string[]>([]);
+
+const votes = ref({
   upvote: {
     icon: 'mdi-arrow-up',
     color: 'primary',
-    active: false,
   },
   downvote: {
     icon: 'mdi-arrow-down',
     color: 'error',
-    active: false,
   },
+})
+
+const actions = ref({
   comment: {
     icon: 'mdi-comment',
     color: 'secondary',
-    active: false,
   },
   favorite: {
     icon: 'mdi-heart',
     color: 'quaternary',
-    active: false,
   }
 })
-
 </script>
--- src/stores/sheet.ts
+++ src/stores/sheet.ts
@@ -1,10 +1,23 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
+import Tag from '@/objects/Tag'
+
 // Create a new store for different categories of sheets: attributes, image, description, actions
 
 export const useSheetStore = defineStore('bottom-sheet', () => {
   const display = ref(false)
+  const data = ref<Tag>()
+
+  function setTag(tagText: string, tagColor: string, tagIcon: string) {
+    data.value = new Tag(tagText, tagColor, tagIcon)
+  }
+
+  function openTag(tagText: string, tagColor: string, tagIcon: string) {
+    console.log('openTag', tagText, tagColor, tagIcon)
+    setTag(tagText, tagColor, tagIcon)
+    show()
+  }
 
   function show() {
     display.value = true
@@ -18,5 +31,5 @@ export const useSheetStore = defineStore('bottom-sheet', () => {
     display.value = !display.value
   }
 
-  return { display, show, hide, toggle }
+  return { display, show, hide, toggle, data, setTag, openTag }
 })

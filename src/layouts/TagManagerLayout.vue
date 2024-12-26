c712301cfb374627a94a801e293307c998e98f3c
nuForge <nuforge@gmail.com>
2024-12-26 14:46:03 -0500

    Tag Layout Refactor

--- src/layouts/TagManagerLayout.vue
+++ src/layouts/TagManagerLayout.vue
@@ -1,7 +1,5 @@
 <template>
   <v-container class="bg-surface rounded-lg elevation-4 me-15  mb-6 ms-2 pa-0  ">
-    <AddTag v-if="state.add" class="bg-surface" />
-    <v-divider v-if="state.add" />
     <tag-group :tags="tags.tags" class="pa-1 bg-background ma-2 rounded" column :noLabel="styles.labels"
       :noIcon="styles.icons" :closable="styles.closable" v-model="tags.selection" />
   </v-container>
@@ -10,13 +8,10 @@
 <script setup lang="ts">
 //
 import { onMounted } from 'vue'
-import { useStateStore } from '@/stores/state'
 import { useStyleStore } from '@/stores/styles'
 import { useTagStore } from '@/stores/tags'
-import AddTag from '@/components/tags/AddTag.vue'
 import TagGroup from '@/components/tags/TagGroup.vue';
 const tags = useTagStore()
-const state = useStateStore()
 const styles = useStyleStore()
 
 onMounted(() => {

53c02888a4ef253730944e4370c2811f1d8420a8
nuForge <nuforge@gmail.com>
2024-12-26 13:34:39 -0500

    story tags

--- src/components/story/StoryChoiceGroup.vue
+++ src/components/story/StoryChoiceGroup.vue
@@ -7,6 +7,7 @@
       </v-col>
     </v-row>
   </v-item-group>
+  <tag-group :tags="story.tags" noLabel noValue v-model="tags.selection" />
 </template>
 
 <script setup lang="ts">
@@ -14,12 +15,17 @@ import { ref } from 'vue'
 import storyChoice from '@/components/story/StoryChoice.vue'
 import { useStoryStore } from '@/stores/story';
 import { useTagStore } from '@/stores/tags';
+import Tag from '@/objects/Tag'
+import TagGroup from '@/components/tags/TagGroup.vue';
 const story = useStoryStore()
 const selection = ref<number>();
 const tags = useTagStore()
 
 const updateSelection = (value: number) => {
-  console.log(`choice:${story.choices[value].tags}`, story.choices[value].color, story.choices[value].icon)
-  tags.addLabel(`choice:${story.choices[value].tags}`, story.choices[value].color, story.choices[value].icon)
+  const choice = story.choices[value]
+  const tag = new Tag(`choice:${choice.tags}`, choice.color, choice.icon)
+  console.log(`choice:${choice.tags}`, choice.color, choice.icon)
+  console.log('tag:', tag)
+  story.tagMap.addTag(tag)
 }
 </script>

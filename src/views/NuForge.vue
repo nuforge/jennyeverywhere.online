e285ec6610f65c87e395661787021547d5014e75
nuForge <nuforge@gmail.com>
2025-01-06 19:24:13 -0500

    Wackiness with card lists

--- src/views/NuForge.vue
+++ src/views/NuForge.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { reactive, computed, ref, watch } from 'vue';
 
 import usePersonaStore from '@/stores/persona';
 const persona = usePersonaStore()
@@ -26,23 +26,29 @@ const colorList = ref<{ [key: string]: string }>({ 'spade': 'primary', 'heart': 
 const ranks = ref(['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'joker'])
 const rankIcons = ref<{ [key: string]: string }>({ 'joker': 'mdi-star', 'ace': 'mdi-chess-bishop', '10': 'mdi-chess-rook', 'jack': 'mdi-chess-knight', 'queen': 'mdi-chess-queen', 'king': 'mdi-chess-king' })
 
-const suitSelect = ref<string>('spade')
-const rankSelect = ref<string>('ace')
+const suitSelect = ref(['spade'])
+const rankSelect = ref(['ace'])
 
-const selectedColor = computed(() => { return colorList.value[suitSelect.value] })
-
+const filteredSuits = computed(() => suitList.value.filter(suit => suitSelect.value.includes(suit)) as string[])
+const filteredRanks = computed(() => ranks.value.filter(rank => rankSelect.value.includes(rank)) as string[])
 
 const DeckOfCards = computed(() => {
+  console.log('                 -                    -                           -')
   const deck: Tag[] = []
   const solid = false
-  for (const suit of suitList.value) {
-    for (const rank of ranks.value) {
+  for (const suit of filteredSuits.value) {
+
+    console.log(suit)
+    for (const rank of filteredRanks.value) {
+      console.log(rank)
       const newIcon = rankIcons.value[rank] ? rankIcons.value[rank] : typeof rank === 'number' ? `mdi-numeric-${rank}-circle` : `mdi-chess-pawn`
       const append = solid ? `${newIcon}` : `${newIcon}` // FUTURE OUTLINE CHECK
       //console.log(append)
-      deck.push(new Tag(`${suit}:${rank}`, colorList.value[suit], append))
+      const tag = new Tag(`${suit}:${rank}`, colorList.value[suit], append)
+      deck.push(tag)
     }
   }
+  console.log(deck)
   return deck
 })
 
@@ -59,20 +65,18 @@ watch(randomNumber.value, () => {
   <v-container>
     <v-row>
       <v-col>
-        <v-btn-toggle v-model="suitSelect">
+        <v-btn-toggle v-model="suitSelect" multiple>
           <v-btn v-for="suit in suitList" :key="suit" :color="colorList[suit]" :icon="`mdi-cards-${suit}`"
             variant="plain" :value="suit" />
         </v-btn-toggle>
-        <v-divider />
-
-        <v-btn-toggle v-model="rankSelect">
-          <v-btn v-for="rank in ranks" :key="rank" :text="rank" :color="selectedColor" variant="plain" size="small">
+        <v-btn-toggle v-model="rankSelect" multiple variant="text" rounded>
+          <v-btn v-for="rank in ranks" :key="rank" :text="rank" size="small" :value="rank">
             <template #append>
-              <v-icon :icon="rankIcons[rank] ? rankIcons[rank] : `mdi-chess-pawn`" :color="selectedColor"></v-icon>
+              <v-icon :icon="rankIcons[rank] ? rankIcons[rank] : `mdi-chess-pawn`"></v-icon>
             </template>
           </v-btn>
         </v-btn-toggle>
-        <v-divider />
+        <v-divider class="my-2" />
         <NuTag v-for="card in DeckOfCards" :key="card.name" :tag="card" :value="(card.space as string)"
           @double-click="persona.focusOn(card)" />
       </v-col>

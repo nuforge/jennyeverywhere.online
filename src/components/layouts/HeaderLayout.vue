0dfbdda72e2d6ba8b39e84195d76cadd659cbb9a
nuForge <nuforge@gmail.com>
2025-01-09 13:19:19 -0500

    color logo changer

--- src/components/layouts/HeaderLayout.vue
+++ src/components/layouts/HeaderLayout.vue
@@ -1,13 +1,18 @@
+<script setup lang="ts">
+import LogoThemed from '@/components/LogoThemed.vue'
+import usePersonaStore from '@/stores/persona';
+import { computed } from 'vue';
+
+const persona = usePersonaStore()
+
+const jennyColor = computed(() => persona.focus.color)
+
+</script>
+
 <template>
   <v-app-bar elevation="0" class="bg-transparent" app scroll-behavior="fade-image">
     <router-link to="/">
-      <img :src="logo" width="200px" class="mx-10" id="header_logo" />
+      <LogoThemed :jenny="jennyColor" />
     </router-link>
   </v-app-bar>
 </template>
-
-<script setup lang="ts">
-import logo from '@/assets/logo.svg'
-
-
-</script>
